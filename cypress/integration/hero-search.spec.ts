921a38561df22bd09caaebb42a713d097de563c5
Farooq Mahmud <farooq.mahmud@marel.com>
2019-11-06 19:20:24 -0800

    More tests

--- cypress/integration/hero-search.spec.ts
+++ cypress/integration/hero-search.spec.ts
@@ -3,14 +3,18 @@ describe('hero search box', () => {
     cy.visit('/dashboard');
   });
 
-  it(`navigates to the hero's URL when search result is clicked`, () => {
-    searchAndClickResult('mr', 'Mr. Nice');
-    cy.get('@clickedSearchResult').click();
-    cy.url().should('contain', '/detail/11');
+  it('shows the expected number of results', () => {
+    cy.get('#search-box').type('n');
+    cy.get('.search-result li').should('have.length', 6);
+
+    cy.get('#search-box').type('a');
+    cy.get('.search-result li').should('have.length', 3);
+
   });
 
-  const searchAndClickResult = (searchText, resultToClick) => {
-    cy.get('#search-box').type(searchText);
-    cy.get('.search-result li').contains(resultToClick).as('clickedSearchResult');
-  }
+  it(`navigates to the hero's URL when search result is clicked`, () => {
+    cy.get('#search-box').type('{backspace}{backspace}mr');
+    cy.get('.search-result li').should('contain', 'Mr. Nice').click();
+    cy.url().should('contain', '/detail/11');
+  });
 });
